8ff2a415d235ae55efbd208f337283156522b85a
Mudassir Fayaz <105848832+Fayaz-khani@users.noreply.github.com>
2024-06-09 15:10:33 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 # Load the tokenizer and model
-# tokenizer = AutoTokenizer.from_pretrained('tokenizer-llama_career_0.5.0')
+tokenizer = AutoTokenizer.from_pretrained('tokenizer-llama_career_0.5.0')
 # model = AutoModelForCausalLM.from_pretrained('llama-2-7b-career-0.5.0')
 
 # Function to generate response
